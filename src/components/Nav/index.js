7337cf26998b04e7fbc2d274ae4e92c82b09201d
MaximeConan <maximeconan5@gmail.com>
2019-03-19 10:54:12 +0100

    <Nav />

---
+++ src/components/Nav/index.js
@@ -0,0 +1,57 @@
+// NPM Import
+import React from 'react'
+import { Menu, Button, Container } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
+
+// Local Import
+import './nav.scss'
+
+// Code
+class Nav extends React.Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Container>
+        <Menu secondary className="nav">
+          <h1>[Brand]</h1>
+          <Menu.Menu position="right">
+            <Menu.Item name="Accueil" active={activeItem === 'Accueil'} onClick={this.handleItemClick} />
+            <Menu.Item
+              as={NavLink}
+              to="/recettes"
+              name="Les recettes"
+              active={activeItem === 'Les recettes'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              as={NavLink}
+              to="/single-recettes"
+              name="Une recette"
+              active={activeItem === 'Une recette'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="Le blog"
+              active={activeItem === 'Le blog'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item as={NavLink} to="/agenda">
+              <Button id="nav-button--calendar">Mon agenda</Button>
+            </Menu.Item>
+            <Menu.Item>
+              <Button id="nav-button--log">Se déconnecter</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </Container>
+    )
+  }
+}
+
+// Export
+export default Nav
