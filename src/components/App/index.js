df5ffb4cfabf558bc9f596390f47189a445c6bf4
MaximeConan <maximeconan5@gmail.com>
2019-03-19 10:52:31 +0100

    <App />

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,37 @@
 /**
  * NPM import
  */
-import React from 'react';
+import React from 'react'
+import { Route } from 'react-router-dom'
 
 /**
  * Local import
  */
-import './app.scss';
+import './app.scss'
+import Nav from 'src/components/Nav'
+import Calendar from 'src/components/Calendar'
+import Recipes from 'src/components/Recipes'
+import Recipe from 'src/components/Recipe'
+import Footer from 'src/components/Footer'
+import Copyright from 'src/components/Copyright'
 
 /**
  * Code
  */
+
 const App = () => (
-  <div id="app">Hello depuis React</div>
-);
+  <div id="app">
+    <Nav />
+    <Route path="/agenda" component={Calendar} />
+    <Route path="/recettes" component={Recipes} />
+    <Route path="/single-recettes" component={Recipe} />
+    <Footer />
+    <Copyright />
+  </div>
+)
+
 
 /**
  * Export
  */
-export default App;
+export default App
