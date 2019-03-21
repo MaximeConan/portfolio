0c81780fe593069955f31cfddbec786496d9ff43
louisodint <louis.odinet@gmail.com>
2019-03-21 15:21:35 +0100

    Intégration de toutes les sections de la Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 // NPM import
-import React from 'react'
-import { Container, Grid } from 'semantic-ui-react'
+import React, { Fragment } from 'react'
+import { Container, Grid, Button } from 'semantic-ui-react'
 import Coverflow from 'react-coverflow'
 
 
@@ -10,46 +10,75 @@ import './home.scss'
 
 // Code
 const Home = () => (
-
-  <Container fluid>
-    <Grid className="section1">
-      <Grid.Column width={9}>
-        <h2>Votre planning hebdo' en quelques clics !</h2>
-        <p>Vous en avez marre de ne pas savoir quoi faire pour le diner ?<br />
-        Nous sommes là pour vous aider !
-        </p>
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <img className="test" src="src/images/Mockup - copie.png" alt="" />
-      </Grid.Column>
-    </Grid>
-    <Grid className="section2">
-      <Coverflow
-        media={{
-          '@media (max-width: auto)': {
-            width: '100vw',
-            height: '80vh',
-          },
-          '@media (min-width: 80vh)': {
-            width: '100vw',
-            height: '70vh',
-          },
-        }}
-        displayQuantityOfSide={2}
-        navigation={false}
-        infiniteScroll={1}
-        enableScroll={1}
-        clickable={1}
-        active={0}
-      >
-        <img src="src/images/sara-dubler-648793-unsplash.jpg" alt="Recette 1" data-action="" />
-        <img src="src/images/eaters-collective-132772-unsplash.jpg" alt="Recette 2" data-action="" />
-        <img src="src/images/brenan-greene-229562-unsplash.jpg" alt="Recette 3" data-action="" />
-        <img src="src/images/brenan-greene-229562-unsplash.jpg" alt="Recette 3" data-action="" />
-        <img src="src/images/rsz_lily-banse-365344-unsplash.jpg" alt="Recette 4" data-action="" />
-      </Coverflow>
-    </Grid>
-
-  </Container>
+  <Fragment>
+    <Container fluid>
+      <Grid className="section1">
+        <Grid.Column width={6} className="section1-right">
+          <h2 className="section1-right-title">Votre planning hebdo' en quelques clics !</h2>
+          <p className="section1-right-text">Vous en avez marre de ne pas savoir quoi faire pour le diner ?<br />
+          Nous sommes là pour vous aider !
+          </p>
+          <Button className="section1-right-button">Je m'inscris</Button>
+        </Grid.Column>
+        <Grid.Column width={7}>
+          <img className="section1-left-image" src="src/assets/Home/section-1-mockup-full.png" alt="" />
+        </Grid.Column>
+      </Grid>
+      <Grid textAlign="center" className="section2">
+        <h2 className="section2-title">Faites votre choix parmi 70 000 recettes.</h2>
+        <Coverflow
+          media={{
+            '@media (max-width: auto)': {
+              width: '100vw',
+              height: '80vh',
+            },
+            '@media (min-width: 80vh)': {
+              width: '100vw',
+              height: '70vh',
+            },
+          }}
+          displayQuantityOfSide={2}
+          navigation={false}
+          infiniteScroll={1}
+          enableScroll={1}
+          clickable={1}
+          active={0}
+        >
+          <img src="src/assets/sara-dubler-648793-unsplash.jpg" alt="Recette 1" data-action="" />
+          <img src="src/assets/eaters-collective-132772-unsplash.jpg" alt="Recette 2" data-action="" />
+          <img src="src/assets/brenan-greene-229562-unsplash.jpg" alt="Recette 3" data-action="" />
+          <img src="src/assets/brenan-greene-229562-unsplash.jpg" alt="Recette 3" data-action="" />
+          <img src="src/assets/rsz_lily-banse-365344-unsplash.jpg" alt="Recette 4" data-action="" />
+        </Coverflow>
+      </Grid>
+      <Grid>
+        <Grid.Column textAlign="center" className="section3">
+          <h2 className="section3-title">Planifiez vos recettes de la semaine.</h2>
+          <img className="section3-image" src="src/assets/agenda copie.png" alt="" />
+        </Grid.Column>
+      </Grid>
+      <Grid className="section4">
+        <h2 className="section4-title">Générez votre liste de courses en 1 clic !</h2>
+      </Grid>
+      <Grid className="section5">
+        <h2 className="section5-title">Cependant nous ne faisons (pas encore) les courses pour vous, mais nous vous accompagnons pour la réalisation des recettes</h2>
+      </Grid>
+      <Grid className="section6">
+        <h2 className="section6-title">Rejoignez-nous en vous inscrivant, le meileur, c'est que c'est totalement gratuit !</h2>
+        <Button>Je m'inscris</Button>
+      </Grid>
+      <Grid className="section7">
+        <h2 className="section7-title">Ils l'ont testé et vite adopté !</h2>
+        <Grid.Column>
+          <img src="" alt=""/>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et modi soluta officiis nihil laboriosam saepe, perspiciatis repudiandae vitae temporibus veritatis porro ad alias quam expedita.</p>
+        </Grid.Column>
+      </Grid>
+      <Grid className="sectionNewsletter">
+        <h2 className="sectionNewsletter-title">[Brand] NEWSLETTER</h2>
+        <p>Inscrivez-vous à notre Newsletter afin de recevoir les denrières recettes de saisons et nos actualités les plus fraîches !</p>
+      </Grid>
+    </Container>
+  </Fragment>
 )
 export default Home
