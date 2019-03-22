1c88243ea2f9f7448c517eb4e4f89a98743fdff3
louisodint <louis.odinet@gmail.com>
2019-03-22 09:45:03 +0100

    Ajout sans mise en page de commentaires

--- src/components/Home/Sections/Section3.js
+++ src/components/Home/Sections/Section3.js
@@ -12,6 +12,8 @@ const Section3 = () => (
   <Grid textAlign="center" className="section3">
     <h2 className="section3-title">Planifiez vos recettes de la semaine.</h2>
     <img className="section3-image" src="src/assets/Home/agenda copie.png" alt="" />
+    <span>Ajoutez facilement vos recettes à votre planning</span>
+    <span>Déplacez-les tout aussi facilement en les faisant glisser</span>
   </Grid>
 )
 
