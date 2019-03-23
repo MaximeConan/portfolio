0b2d616888a71f907afc524d1e5a32172d30eafe
louisodint <louis.odinet@gmail.com>
2019-03-23 14:40:35 +0100

    Responsivité de la ShoppingList : done

--- src/components/ShoppingList/index.js
+++ src/components/ShoppingList/index.js
@@ -2,13 +2,15 @@
 * NPM import
 */
 import React from 'react'
-import { Grid, Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 /**
 * local import
 */
 
 // Import(s) du/des component(s)
 import HeroSection from 'src/components/Calendar/HeroSectionCalendar'
+import ShoppingListWeb from './ShoppingListWeb'
+import ShoppingListMobile from './ShoppingListMobile'
 // import ShoppingListItems from 'src/components/ShoppingList/ShoppingListItems/index'
 
 // Style SCSS
@@ -17,171 +19,13 @@ import './shoppingList.scss'
 // Import Data
 import data from 'src/data/recipes'
 
+
 // Code
 const ShoppingList = () => (
   <Container fluid>
     <HeroSection />
-    <Container>
-      <Grid>
-        <Grid.Row className="shoppingList">
-          <Grid.Column width={3} className="shoppingList-column">
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Légumes</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  {/* <li><input type="checkbox" />
-                    {data.recipeIngredients.map(ingredient => (
-                      <ShoppingListItems
-                        ingredients={ingredient}
-                      />
-                    ))}
-                  </li> */}
-                  <li><input type="checkbox" />test</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Fruits</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-          </Grid.Column>
-          {/* <Grid.Column width={3} className="shoppingList-column">
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Biscuits et gâteaux</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Crémerie</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-          </Grid.Column>
-          <Grid.Column width={3} className="shoppingList-column">
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Viandes et poissons</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Pâtes et riz</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-          </Grid.Column>
-          <Grid.Column width={3} className="shoppingList-column">
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Conserves</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Condiments</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Boulangerie</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-          </Grid.Column>
-          <Grid.Column width={3} className="shoppingList-column">
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Petit déjeuner</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Boissons</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-            <div className="shoppingList-theme">
-              <h2 className="shoppingList-title">Autres</h2>
-              <div className="shoppingList-cell">
-                <ul>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                  <li><input type="checkbox" />1kg de carotte</li>
-                </ul>
-              </div>
-            </div>
-          </Grid.Column> */}
-          <div>
-            <a href="#" className="shoppingList-footer-button">Exportez votre liste de courses</a>
-          </div>
-        </Grid.Row>
-      </Grid>
-    </Container>
+    <ShoppingListWeb />
+    <ShoppingListMobile />
   </Container>
 )
 
